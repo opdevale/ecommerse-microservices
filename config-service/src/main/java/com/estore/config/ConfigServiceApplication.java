742304ffdd5535c8e5fd15e197e64ad8e7656ff2
Onkar Devale <opdevale@gmail.com>
2021-12-27 20:24:03 +0530

    config file changes

--- config-service/src/main/java/com/estore/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/estore/config/ConfigServiceApplication.java
@@ -2,6 +2,7 @@ package com.estore.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,10 +12,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class APIGatewayServiceApplication {
+@EnableConfigServer
+public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(APIGatewayServiceApplication.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
